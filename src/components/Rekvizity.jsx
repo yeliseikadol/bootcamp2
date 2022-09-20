4c943ab0d0cca8f50fe06fb2dc4e33b5406d650c
Yelisei Kadol <yeliseikadol@gmail.com>
2022-09-20 14:11:46 +0300

    message

--- src/components/Rekvizity.jsx
+++ src/components/Rekvizity.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
+import Section from './Section'
+import SectionHeader from './SectionHeader'
 
 function Rekvizity() {
     return (
-        <div>
-            <h1>Реквизиты для оплаты</h1>
-            <div>УНП 193340424</div>
-            <div>ИП КАДОЛ ЕЛИСЕЙ НИКОЛАЕВИЧ</div>
-            <div>ALFABY2X ЗАО «Альфа-Банк»</div>
-            <div>BY92ALFA30132585760010270000</div>
-            <div>Назначение платежа: Оплата обучения. Имя Фамилия.</div>
+        <Section>
+            <SectionHeader
+                title="Реквизиты для оплаты"
+            />
+            <div>
+                <div>УНП 193340424</div>
+                <div>ИП КАДОЛ ЕЛИСЕЙ НИКОЛАЕВИЧ</div>
+                <div>ALFABY2X ЗАО «Альфа-Банк»</div>
+                <div>BY92ALFA30132585760010270000</div>
+                <div>Назначение платежа: Оплата обучения. Имя Фамилия.</div>
+            </div>
             <div><a href='/'>Вернуться на главную страницу</a></div>
-        </div>
+        </Section>
     )
 }
 
