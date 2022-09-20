f7b288f14c36765993dfc3ad1129a83e4e032703
Yelisei Kadol <yeliseikadol@gmail.com>
2022-09-20 15:44:51 +0300

    message

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,8 +4,12 @@ export default function Button({props, children}) {
 
   const type = props
 
+  const onClick = () => {
+    fbq('track', 'Lead', {currency: "USD", value: 600.00});
+  };
+
   return (
-    <button type={type}>
+    <button type={type} onClick={onClick}>
       {children}
     </button>
   )
