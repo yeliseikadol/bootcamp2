7e55ba6d2df761c5527427834d94e0a48a19306a
Yelisei Kadol <yeliseikadol@gmail.com>
2022-10-01 20:52:13 +0300

    message

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -29,22 +29,45 @@ export default function Form() {
         method="POST" 
         data-netlify="true">
         <input type="hidden" name="form-name" value="contact" />
-        <div className='row phone-input'>
-            <input
-                  name='promocode'
+        <div className='flex-column input-container'>
+          
+          <label for='name' className='input-label'>Имя</label>
+          <input
+                  name='name'
                   type='text'
-                  placeholder="Прокомод"
+                  placeholder=""
                   />
         </div>
-        <div className='row phone-input'>
-            <div className='prefix'>+375</div>
+        
+        <div className='flex-column input-container'>
+        <label for='tel' className='input-label'>Телефон</label>
             <input
                 name='tel'
                 type='tel'
-                placeholder="Введите телефон"
+                placeholder="+375 12 123-45-67"
                 required
                 />
             </div>
+            <div className='flex-column input-container'>
+            <label form='class' className='input-label'>Группа</label>
+            <select
+                name='class'
+                placeholder="Выберите группу"
+                required>
+                  <option value='yelisei-oct3'>Елисей (3 окт — 23 дек; Пн, Ср 19:00)</option>
+                  <option value='oksana-nov21'>Оксана (21 ноя — 10 фев; Вт, Чт 19:00)</option>
+                  <option value='yelisei-nov21'>Елисей (21 ноя — 10 фев; Пн, Ср 17:00)</option>
+            </select>
+            </div>
+            <div className='flex-column input-container'>
+          
+          <label for='promocode' className='input-label'>Промокод</label>
+          <input
+                  name='promocode'
+                  type='text'
+                  placeholder=""
+                  />
+        </div>
         <Button type='submit'>Записаться</Button>
         <p className='typography-body2'>После нажатия на кнопку с вами свяжется менеджер. Мы работаем по будням с 10 до 18.</p>
       </form>
