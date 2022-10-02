a8efb43e3d44bd6ebbd7270ab354e3db4f5b4afd
Yelisei Kadol <yeliseikadol@gmail.com>
2022-10-02 11:45:35 +0300

    message

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Section from './Section'
-import { Link, animateScroll as scroll } from "react-scroll";
+import { Link } from "react-scroll";
 
 
 export default function HeroSection() {
